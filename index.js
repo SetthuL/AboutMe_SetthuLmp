262f4e100898aeabc64c7233939063c2f66dcb46
Lange Siphosethu <128488997+SetthuL@users.noreply.github.com>
2025-02-06 19:00:09 +0200

    copied an existing project, that was private

---
+++ index.js
@@ -0,0 +1,16 @@
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+scrollToTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
